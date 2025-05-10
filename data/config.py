f48e7bc199ade057f96f3710ea6e594c74ed8e91
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-10 16:11:46 +0500

    Commited

--- data/config.py
+++ data/config.py
@@ -6,6 +6,7 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 ADMIN_GROUP = env.str("ADMIN_GROUP")
+REDIS_PASS = env.str("REDIS_PASS")
 IP = env.str("IP")
 
 DB_USER = env.str("DB_USER")
--- loader.py
+++ loader.py
@@ -2,13 +2,14 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from data import config
+from data.config import REDIS_PASS
 from utils.db_api.books import BooksDB
 from utils.db_api.main_db import Database
 from utils.db_api.students import StudentsDB
 from utils.db_api.users_admins import UsersAdminsDB
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2('localhost', 6379, state_ttl=3000, data_ttl=3000)
+storage = RedisStorage2(host='localhost', port=6379, password=REDIS_PASS, state_ttl=3000, data_ttl=3000)
 dp = Dispatcher(bot, storage=storage)
 db = Database()
 udb = UsersAdminsDB(db)
